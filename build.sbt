ab67fe554fd3bf24dc29b58443dcf3a78454bb8c
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2018-03-18 14:50:51 +0900

    サンプル追加

--- build.sbt
+++ build.sbt
@@ -2,9 +2,11 @@ name := """akka-backoff-supervisor-sample"""
 
 version := "1.0"
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.3.11",
-  "com.typesafe.akka" %% "akka-testkit" % "2.3.11" % "test",
-  "org.scalatest" %% "scalatest" % "2.2.4" % "test")
+  "com.typesafe.akka" %% "akka-actor" % "2.4.9",
+  "com.typesafe.akka" %% "akka-testkit" % "2.4.9" % "test",
+  "com.typesafe.akka" %% "akka-slf4j" % "2.4.9",
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+)
--- src/main/scala/com/example/ApplicationMain.scala
+++ src/main/scala/com/example/ApplicationMain.scala
@@ -1,12 +1,58 @@
 package com.example
 
-import akka.actor.ActorSystem
+import akka.actor.Actor.Receive
+import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, PoisonPill, Props }
+import akka.pattern.{ Backoff, BackoffSupervisor }
+
+import scala.concurrent.duration.FiniteDuration
+import scala.util.control.NoStackTrace
+import scala.concurrent.duration._
+
+class TestException extends RuntimeException with NoStackTrace
+
+object PrintActor {
+  def props: Props = Props(new PrintActor)
+}
+
+class PrintActor extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case "boom" => throw new TestException
+    case msg: String  =>
+      log.info("Receive message: {}", msg)
+  }
+
+  @scala.throws[Exception](classOf[Exception])
+  override def postStop(): Unit = {
+    super.postStop()
+    log.info("post stop")
+  }
+
+  @scala.throws[Exception](classOf[Exception])
+  override def postRestart(reason: Throwable): Unit = {
+    super.postRestart(reason)
+    log.info("post restart")
+  }
+}
 
 object ApplicationMain extends App {
   val system = ActorSystem("MyActorSystem")
-  val pingActor = system.actorOf(PingActor.props, "pingActor")
-  pingActor ! PingActor.Initialize
-  // This example app will ping pong 3 times and thereafter terminate the ActorSystem - 
-  // see counter logic in PingActor
-  system.awaitTermination()
+
+  val supervisor = system.actorOf(
+    BackoffSupervisor.props(
+      Backoff.onFailure(
+        childProps = PrintActor.props,
+        childName = "print-actor",
+        minBackoff = 100.millis,
+        maxBackoff = 3.seconds,
+        randomFactor = 0.2
+      )
+    )
+  )
+
+  supervisor ! PoisonPill
+
+
+
+  Thread.sleep(1000)
+  system.terminate()
 }
